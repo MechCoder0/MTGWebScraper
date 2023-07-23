72f5f2fb16021237bf9b90374d74027beaec340d
= <icedragonzclawz@gmail.com>
2023-07-23 11:41:14 -0400

    Updated main script to check if the decks should be scraped from the web and if so, writes the html to a file

--- webScraper.py
+++ webScraper.py
@@ -4,8 +4,14 @@ from deck import Deck
 from fileutil import get_deck_paths
 import sys
 
-if len(sys.argv) > 1 and sys.argv == "web":
+def should_get_from_web():
+    return len(sys.argv) > 1 and sys.argv[1] == "web"
+
+should_scrape_web = should_get_from_web()
+
+if should_scrape_web:
     deck_paths = DeckUtil.get_decks_from_web()
+    deck_paths = deck_paths[:1]
 else:
     deck_paths = get_deck_paths()
 
@@ -13,7 +19,8 @@ card_dict = {}
 for deck_path in deck_paths:
     deck_soup = get_soup(deck_path, True)
     deck = Deck(deck_soup)
-    # deck.write_deck_to_file()
+    if should_scrape_web:
+        deck.write_deck_to_file()
     
     print("Getting cards for: " + deck.get_title())
     for card in deck.get_cards():
