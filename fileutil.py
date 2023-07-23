30da05496ade8ef702ee7cbc2f01158674e5100d
= <icedragonzclawz@gmail.com>
2023-07-23 11:57:50 -0400

    added creating the website folder if it doesn't exist

--- fileutil.py
+++ fileutil.py
@@ -1,6 +1,10 @@
 from glob import glob
+import os
 
 def write_to_file(file_name, content):
+    is_dir_created = os.path.isdir('websites')
+    if not is_dir_created:
+        os.makedirs("websites")
     file = open('websites/' + file_name, 'w', encoding='utf-8')
     file.writelines(content)
     file.close()
